5ebd642f0836acdbfdad8b5a294017e428c45862
Dominic Cyr <cyrd97@gmail.com>
2023-01-05 21:41:17 -0500

    Update total-support-calculator.js
    Supporter plusieurs tables et adapter le css

--- total-support-calculator.js
+++ total-support-calculator.js
@@ -1,27 +1,32 @@
 javascript: (() => {
-	if (window.hasCalculatedTotalTroops){
-		return;
-	}
+	const tbodys = $("tbody:has(>tr>th:has(>img[src*='spear.png'],>a>img[src*='spear.png']))");
 	
-	window.hasCalculatedTotalTroops = true;
-	const tbody = $("form#withdraw_selected_units_village_info table.vis tbody")[0];
-	
-	let rowCount = tbody.children.length - 2;
-	let columnCount = tbody.children[0].children.length - 2;
-	
-	let rowContent = [];
-	
-	for (let col = 0; col <= columnCount; col++) {
-		if (col == 0) {
-			rowContent.push("Total troops: ");
-		} else {
-			let total = 0;
-			for (let row = 1; row <= rowCount; row++) {
-				total += Number(tbody.children[row].children[col].innerHTML);
+	tbodys.each(function(index, tbody) {
+		if($(tbody).data("hasTotal")) return;
+		
+		$(tbody).data("hasTotal", true);
+		
+		const isLastRowValid = !isNaN(tbody.children[tbody.children.length-1].children[1].innerHTML.trim());
+		const isLastColValid = !isNaN(tbody.children[1].children[tbody.children[1].children.length-1].innerHTML.trim());
+		const style = $(tbody.children[1].children[1]).attr("style");
+		
+		let rowCount = tbody.children.length - (isLastRowValid ? 1 : 2);
+		let columnCount = tbody.children[0].children.length - (isLastColValid ? 1 : 2);
+		
+		let rowContent = [];
+		
+		for (let col = 0; col <= columnCount; col++) {
+			if (col == 0) {
+				rowContent.push("Total troops: ");
+			} else {
+				let total = 0;
+				for (let row = 1; row <= rowCount; row++) {
+					total += Number(tbody.children[row].children[col].innerHTML);
+				}
+				rowContent.push("" + total);
 			}
-			rowContent.push("" + total);
 		}
-	}
-	
-	$(tbody.children).eq(rowCount).after("<tr>" + rowContent.map((x, i) => (i == 0 ? "<td>" : "<td style=\"text-align:center\">") + x + "</td>").join('') + "</tr>");
+		
+		$(tbody.children).eq(rowCount).after("<tr>" + rowContent.map((x, i) => (i == 0 ? "<td>" : "<td style=\"" + style + "\"" + (x == "0" ? "class=\"hidden\"" : "") + ">") + x + "</td>").join('') + "</tr>");
+	});
 })();
